2f1515fa86402108cc3885c1a53a2f0ba5353cf8
mckeowr1 <mckeow.jr1@gmail.com>
2025-05-07 14:40:14 -0500

    local_test run

--- test_local.sh
+++ test_local.sh
@@ -1,2 +1,2 @@
 nextflow run main.nf -profile local \
---vcf /vast/eande106/data/c_elegans/WI/variation/20220216/vcf/WI.20220216.hard-filter.isotype.vcf.gz
+--vcf /Users/ryanmckeown/Desktop/finemap_qtl_overlap/WI.20220216.hard-filter.isotype.vcf.gz
